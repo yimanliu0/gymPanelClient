d757a71d9e8b105b022df828ac58187db52c09c2
Rujun Yao <pandalv9999@gmail.com>
2020-10-20 00:54:03 -0700

    initial commit for login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import LoginForm from "./LoginForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [loggedInUser, setLoggedInUser] = useState();
+
+    const onUserLoggedIn = (user) => {
+        setLoggedInUser(user)
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>Hello World</h2>
+                <p>Here is a sample Header and should be Replace by a actual header</p>
+            </header>
+            {loggedInUser ? <p>Successful Logged In {loggedInUser.username}</p> : <LoginForm userSetter={onUserLoggedIn}/>}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/LoginForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import axios from 'axios';
+
+const LoginForm = ({userSetter}) => {
+
+    const [loginError, setLoginError] = useState("");
+
+    const onLoginClicked = () => {
+        const url = "./" + document.getElementById("username").value;
+        axios.get(url).then(res => {
+            console.log("success");
+            userSetter(res.data)
+        }).catch(err => {
+            console.log("error");
+            setLoginError(err.response.data)
+         });
+    };
+
+    return (
+        <div id="login-form">
+            <label htmlFor="username">Username:</label>
+            <input id="username" name="username" type="text"/>
+            <button id="login-button" onClick={onLoginClicked}>Login</button>
+            <button id="register-button">Register</button>
+            <p id="login-error">{loginError}</p>
+        </div>
+
+    )
+};
+
+export default LoginForm
